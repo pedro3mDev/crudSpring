d310d72dbbd8b636bde96e4c98ab56a3ad600579
Pedro Moniz <88067976+pedro3mDev@users.noreply.github.com>
2024-06-24 09:02:01 +0100

    ...

--- src/main/java/com/grupo3m/crudSpring/repository/CarroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.grupo3m.crudSpring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.grupo3m.crudSpring.entity.Carro;
-
-public interface CarroRepository extends JpaRepository<Carro, Long> {
-
-}
